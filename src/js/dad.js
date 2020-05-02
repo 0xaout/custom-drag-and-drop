1dc91092e8801ff5e82f596a4998b60e3efd2906
bobidiboo <nrobert57@protonmail.com>
2020-05-02 18:17:18 +0200

    updated variables names to make the code more comprehensible

--- src/js/dad.js
+++ src/js/dad.js
@@ -1,34 +1,32 @@
-let mouseX = null;
-let mouseY = null;
-let mouseDown = false;
+let is_mouse_down = false;
 
-let offsetX = null;
-let offsetY = null;
+let element_offset_x = null;
+let element_offset_y = null;
 
 let moving_element = null;
 
-let element_down = false;
-let element_up = false;
+let is_element_lower_side = false;
+let is_element_upper_side = false;
 
-let blockBellow = null;
+let dad_block_bellow = null;
 
 let placeholder = document.createElement("div");
 placeholder.classList.add("placeholder");
 placeholder.classList.add("dad_block");
 
 function dadBlock() {
-    let element = document.createElement("div");
-    element.classList.add("dad_block");
-    element.innerHTML = moving_element.innerHTML;
+    let block = document.createElement("div");
+    block.classList.add("dad_block");
+    block.innerHTML = moving_element.innerHTML;
 
-    return element;
+    return block;
 }
 
 // MOUSE DOWN
 document.addEventListener("mousedown", function(e) {   
     if(e.target.classList.contains("dad_block")) {
-        // activate the mouseDown state
-        mouseDown = true;
+        // activate the is_mouse_down state
+        is_mouse_down = true;
         
         // when a ded_block is pressed, we add a "moving" class to it
         e.target.classList.add("moving");
@@ -39,57 +37,58 @@ document.addEventListener("mousedown", function(e) {
         // for getting the mouse position relative to the clicked div
         // used for div movement when the mouse is down
         let rect = e.target.getBoundingClientRect();
-        offsetX = e.clientX - rect.left;
-        offsetY = e.clientY - rect.top;
+        element_offset_x = e.clientX - rect.left;
+        element_offset_y = e.clientY - rect.top;
 
         placeholder.innerHTML = moving_element.innerHTML;
 
         moving_element.parentNode.insertBefore(placeholder, moving_element);
         moving_element.style.position = "absolute";
-        moving_element.style.left = e.clientX - offsetX + "px";
-        moving_element.style.top = e.clientY - offsetY + "px";
+        moving_element.style.left = e.clientX - element_offset_x + "px";
+        moving_element.style.top = e.clientY - element_offset_y + "px";
     }
 });
 
 // MOUSE MOVE
 document.addEventListener("mousemove", function(e) {
-    if(mouseDown == true) {
+    if(is_mouse_down == true) {
         // changing dragged block style to make it follow the mouse
-        moving_element.style.left = e.clientX - offsetX + "px";
-        moving_element.style.top = e.clientY - offsetY + "px";
+        moving_element.style.left = e.clientX - element_offset_x + "px";
+        moving_element.style.top = e.clientY - element_offset_y + "px";
         
         // checking the div under the dragged block
         moving_element.hidden = true;
         
-        divBellow = document.elementFromPoint(event.clientX, event.clientY).closest(".dad_block_container");
-        blockBellow = document.elementFromPoint(event.clientX, event.clientY).closest(".dad_block");
+        dad_container_bellow = document.elementFromPoint(event.clientX, event.clientY).closest(".dad_block_container");
+        dad_block_bellow = document.elementFromPoint(event.clientX, event.clientY).closest(".dad_block");
         moving_element.hidden = false;
 
-        if(divBellow.children[0] == undefined || divBellow.children[0].classList.contains("moving")) {
-            let element = dadBlock();
+        if(dad_container_bellow.children[0] == undefined || dad_container_bellow.children[0].classList.contains("moving")) {
+            let block = dadBlock();
 
-            divBellow.appendChild(placeholder);
+            dad_container_bellow.appendChild(placeholder);
         }
+
     
-        if(blockBellow != null) {
-            rect = blockBellow.getBoundingClientRect();
+        if(dad_block_bellow != null) {
+            rect = dad_block_bellow.getBoundingClientRect();
 
             placeholder.innerHTML = moving_element.innerHTML;
         
-            if(rect.top + (rect.height / 2) > e.clientY && blockBellow.classList.contains("dad_block")) {
-                element_up = true;
-                element_down = false;
+            if(rect.top + (rect.height / 2) > e.clientY && dad_block_bellow.classList.contains("dad_block")) {
+                is_element_upper_side = true;
+                is_element_lower_side = false;
 
             } else {
-                element_down = true;
-                element_up = false;
+                is_element_lower_side = true;
+                is_element_upper_side = false;
             }
 
-            if(element_up) {
-                blockBellow.parentNode.insertBefore(placeholder, blockBellow); 
+            if(is_element_upper_side) {
+                dad_block_bellow.parentNode.insertBefore(placeholder, dad_block_bellow); 
 
-            } else if(element_down) {
-                blockBellow.parentNode.insertBefore(placeholder, blockBellow.nextSibling); 
+            } else if(is_element_lower_side) {
+                dad_block_bellow.parentNode.insertBefore(placeholder, dad_block_bellow.nextSibling); 
             } 
         }
     }
@@ -98,39 +97,39 @@ document.addEventListener("mousemove", function(e) {
 
 // MOUSE UP
 document.addEventListener("mouseup", function(e) {
-    mouseDown = false;
+    is_mouse_down = false;
     moving_element.classList.remove("moving");
     
     try {
         // if the block is dropped in container, we move this block to the new container
 
-        if(blockBellow != null) {
-            if(element_up) {
-                let element = dadBlock();
+        if(dad_block_bellow != null) {
+            if(is_element_upper_side) {
+                let block = dadBlock();
         
-                blockBellow.parentNode.insertBefore(element, blockBellow); 
-            } else if(element_down) {
-                let element = dadBlock();
+                dad_block_bellow.parentNode.insertBefore(block, dad_block_bellow); 
+            } else if(is_element_lower_side) {
+                let block = dadBlock();
         
-                blockBellow.parentNode.insertBefore(element, blockBellow.nextSibling); 
+                dad_block_bellow.parentNode.insertBefore(block, dad_block_bellow.nextSibling); 
             }
         }
 
-        else if(divBellow.classList.contains("dad_block_container")) {
-            let element = dadBlock();
+        else if(dad_container_bellow.classList.contains("dad_block_container")) {
+            let block = dadBlock();
     
-            divBellow.appendChild(element); 
+            dad_container_bellow.appendChild(block); 
         }
 
     } catch (error) {
         // if the block is not dropped in a container, it goes back in it's previous position
-        let element = dadBlock();
+        let block = dadBlock();
 
-        placeholder.parentNode.insertBefore(element, placeholder);
+        placeholder.parentNode.insertBefore(block, placeholder);
     }
     
     moving_element.remove();
     placeholder.remove();
     moving_element = null;
-    divBellow = null;
+    dad_container_bellow = null;
 });
